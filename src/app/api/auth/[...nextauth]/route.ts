c0a4a171f965f09ee767afb6d73fc36c4eeb817c
Eduardo Daniel de Moura <eduardo.moura@gran.com>
2025-05-28 08:44:01 -0300

    NextAuth com prisma

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,4 +1,4 @@
-import { prisma } from "@/app/lib/prisma";
+import prisma from "@/app/lib/prisma";
 import NextAuth from "next-auth";
 import CredentialsProvider from "next-auth/providers/credentials";
 import GitHubProvider from "next-auth/providers/github";
