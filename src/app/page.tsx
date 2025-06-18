6a3b5783e87788050820bf872bed307a8a910b3f
BrunoOuriquesMeifert <brunoomeifert@aluno.santoangelo.uri.br>
2025-06-17 21:13:50 -0300

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 const links = [
   { name: 'login', href: '/login' },
   { name: 'painel', href: '/painel' },
-  { name: 'filmes', href: '/painel/filmes' },
+  { name: 'filmes', href: '/filmes' },
 ]
 
 export default function Page() {
